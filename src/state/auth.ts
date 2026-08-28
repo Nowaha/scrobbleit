43a498d157bfdb4cd1f7267219d70ccea8afff46
Nowaha <noahverkaik@gmail.com>
2026-08-28 18:42:41 +0200

    Add persisted states for auth

---
+++ src/state/auth.ts
@@ -0,0 +1,8 @@
+import { createPersistedSignal } from "../util/persistedSignal.js";
+
+export const lastFmKey = createPersistedSignal("lastFmKey", null);
+export const lastFmName = createPersistedSignal("lastFmName", null);
+export const lastFmSubscriber = createPersistedSignal("lastFmSubscriber", null);
+export const lastFmUrl = createPersistedSignal("lastFmUrl", null);
+export const lastFmImage = createPersistedSignal("lastFmImage", null);
+export const authTimeStamp = createPersistedSignal("lastFmAuthTimestamp", null);
