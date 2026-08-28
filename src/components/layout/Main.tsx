f72fc148ddda98f3e220654ce3ed17eb3001cec7
Nowaha <noahverkaik@gmail.com>
2026-08-28 18:57:26 +0200

    Only show login section if not logged in

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,4 +1,6 @@
+import { lastFmKey } from "../../state/auth.js";
 import AccountSection from "../account/AccountSection.js";
+import Show from "../basic/Show.js";
 import InfoSection from "../info/InfoSection.js";
 import LoginSection from "../login/LoginSection.js";
 import ManualSection from "../manual/ManualSection.js";
@@ -7,10 +9,11 @@ import SearchSection from "../search/SearchSection.js";
 const Main = () => (
   <main class="flex w-full max-w-2xl grow flex-col gap-8 px-8 py-8">
     <InfoSection />
-    <LoginSection />
-    <AccountSection />
-    <SearchSection />
-    <ManualSection />
+    <Show when={lastFmKey} fallback={<LoginSection />}>
+      <AccountSection />
+      <SearchSection />
+      <ManualSection />
+    </Show>
   </main>
 );
 
